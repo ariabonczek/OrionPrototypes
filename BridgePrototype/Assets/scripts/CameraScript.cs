73ba8439aaab41cc9c0b184a62e14f0895befc4e
unknown <jsl9284@IGMResearch-06.main.ad.rit.edu>
2015-10-26 17:52:36 -0400

    Camera updated, need to account for linear stacking

--- BridgePrototype/Assets/scripts/CameraScript.cs
+++ BridgePrototype/Assets/scripts/CameraScript.cs
@@ -6,10 +6,8 @@ public class CameraScript : MonoBehaviour {
 	public GameObject player1;
     public GameObject player2;
 
-	private Vector3 offset;
     private Vector3 median;
 	private Vector3 cross;
-	private Vector3 newPos;
 
 	private Vector3 velocity = Vector3.zero;
 
@@ -20,8 +18,8 @@ public class CameraScript : MonoBehaviour {
 	private float distanceY;
 	private float distanceZ;
 	private float distance;
-
-	private Quaternion rotation;
+	private float upperLimit;
+	private float lowerLimit;
 
     //CAMERA IMPLEMENTATION TO WORK AS FOLLOWS:
     //---Calculate displacement between players
@@ -35,6 +33,8 @@ public class CameraScript : MonoBehaviour {
 	void Start ()
 	{
         // offset = transform.position - median;
+		upperLimit = 15f;
+		lowerLimit = 2f;
 	}
 	
 	// Update is called once per frame
@@ -47,32 +47,24 @@ public class CameraScript : MonoBehaviour {
 		
 		distanceX = Mathf.Pow((player2.transform.position.x - player1.transform.position.x), 2);
 		distanceY = Mathf.Pow((player2.transform.position.y - player1.transform.position.y), 2);
-		distanceZ = Mathf.Pow((player2.transform.position.z - player1.transform.position.z), 2);
-		
+		distanceZ = Mathf.Pow((player2.transform.position.z - player1.transform.position.z), 2);		
 		distance = Mathf.Sqrt(distanceX + distanceY + distanceZ);
-		
-		this.transform.position = new Vector3(-distance, distance, medZ);
-		
-		// rotation displacement
-		//cross = Vector3.Cross(median, player1.transform.forward);
-		Debug.DrawLine(player1.transform.position, median, Color.black);
-		Debug.DrawLine(player2.transform.position, median, Color.black);
-		Debug.DrawLine(player1.transform.position, player1.transform.forward, Color.green);
-		Debug.DrawLine(transform.position, median, Color.green);
 
-		//cross.y = 0.5f;
-		//Debug.DrawLine(median, cross, Color.red);
-		//Debug.DrawLine(player1.transform.position, cross, Color.red);
-		// this.transform.forward = cross;
+		Vector3 dispalcement = player2.transform.position - player1.transform.position;
+		cross = Vector3.Cross(dispalcement, player1.transform.up);
 
-		//transform.forward = cross.normalized;
+		Debug.DrawLine(median, cross + median, Color.black);
+		Debug.DrawLine(player1.transform.position, cross + median, Color.green);
 
-		/*transform.position = player.transform.position + offset;
-		transform.forward = player.transform.forward;
+		transform.forward = cross;
 
-		transform.RotateAround (player.transform.position, Vector3.up, 1);
-		transform.position = offset.magnitude * -player.transform.forward + player.transform.position;
-		transform.position += new Vector3 (0, 2, 0);*/
-		//transform.rotation = new Quaternion (player.transform.position.x, 20, player.transform.position.z, 0);
+		Vector3 camDistance = cross* -distance;
+		camDistance = Vector3.ClampMagnitude(camDistance, upperLimit);
+		Debug.Log(camDistance.magnitude);
+		transform.position = median + camDistance;
+
+		//transform.position = new Vector3(
+		//	median + (cross* -distance/4)
+		transform.position += transform.up * 2f;
 	}
 }
