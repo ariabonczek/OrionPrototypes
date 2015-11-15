a51e041294eaa0f19accada8e8270af9b9a4fc93
Sam Handrick <sbh9278@rit.edu>
2015-11-14 21:19:34 -0500

    Added in the perspective point camera system,
    with succesful checking for nearest PPoint and
    lerping between the last point and the current.

--- BridgePrototype/Assets/scripts/CameraScript.cs
+++ BridgePrototype/Assets/scripts/CameraScript.cs
@@ -9,13 +9,13 @@ public class CameraScript : MonoBehaviour {
     private Vector3 median;
 	private Vector3 cross;
 	private float camDistance;
-
-	private Vector3 velocity = Vector3.zero;
+	private GameObject closestPoint;
+	private GameObject prevClosestPoint;
 
 	private float medX;
 	private float medY;
 	private float medZ;
-	private float distanceX;
+	/*private float distanceX;
 	private float distanceY;
 	private float distanceZ;
 	private float distance;
@@ -23,7 +23,13 @@ public class CameraScript : MonoBehaviour {
 	private float lowerLimit;
 	private float cameraAngle;
 	private float yVal;
-	private float xzVal;
+	private float xzVal;*/
+
+	private float timerStart;
+	private float timer;
+	private float lerpTime;
+
+	private bool lerping;
 
 	public Vector3 Median
 	{
@@ -31,20 +37,16 @@ public class CameraScript : MonoBehaviour {
 	}
 
     //CAMERA IMPLEMENTATION TO WORK AS FOLLOWS:
-    //---Calculate displacement between players
-    //---Cross displacement with up vector
-    //---Set magnitude of this third vector to be set distance away; this is Camera position(not including y)
-    //---Lerp camera from current position to new position
-    //---Do math stuff to calculate height/camera arm based on displacement length
-    //---do math stuff to calculate camera angle
+    //---Calculate closest Perspective Point
+    //---Set closest Perspective Point's direction as forward.
+    //---Set camera position to be Perspective points specified distance from the player midepiont in the direction of the negated PP direction
+    //---If it find a different closest point than the one it is using then it enters lerp mode
 
 	// Use this for initialization
 	void Start ()
 	{
-		cameraAngle = Mathf.PI / 10;
-        // offset = transform.position - median;
-		upperLimit = 12f;
-		lowerLimit = 3f;
+		lerpTime = 2f;
+		lerping = false;
 	}
 	
 	// Update is called once per frame
@@ -54,39 +56,49 @@ public class CameraScript : MonoBehaviour {
 		medY = (player1.transform.position.y + player2.transform.position.y) / 2;
 		medZ = (player1.transform.position.z + player2.transform.position.z) / 2;
 		median = new Vector3(medX, medY, medZ);
-		
-		distanceX = Mathf.Pow((player2.transform.position.x - player1.transform.position.x), 2);
-		distanceY = Mathf.Pow((player2.transform.position.y - player1.transform.position.y), 2);
-		distanceZ = Mathf.Pow((player2.transform.position.z - player1.transform.position.z), 2);		
-		distance = Mathf.Sqrt(distanceX + distanceY + distanceZ);
 
-		Vector3 dispalcement = player2.transform.position - player1.transform.position;
-		cross = Vector3.Normalize(Vector3.Cross(dispalcement, player1.transform.up));
+		if (lerping) {
+			timer = (Time.time - timerStart);
+			if(timer<lerpTime){
+				Vector3 oldPos = median + -(prevClosestPoint.GetComponent<PPointScript> ().direction * prevClosestPoint.GetComponent<PPointScript> ().distance);
+				Vector3 newPos = median + -(closestPoint.GetComponent<PPointScript> ().direction * closestPoint.GetComponent<PPointScript> ().distance);
 
-		//Debug.DrawLine(median, cross + median, Color.black);
-		//Debug.DrawLine(player1.transform.position, cross + median, Color.green);
+				transform.position = Vector3.Lerp(oldPos, newPos, timer/lerpTime);
 
-		transform.forward = cross;
+				transform.forward = Vector3.Lerp(prevClosestPoint.GetComponent<PPointScript> ().direction, closestPoint.GetComponent<PPointScript> ().direction, timer/lerpTime);
+			}else{
+				lerping= false;
+			}
+		}
 
-		camDistance = distance * 2f;
-		camDistance = Mathf.Clamp(camDistance, lowerLimit, upperLimit);
-		// we constrain to the upper limit, but we don't handle the lower limit, if they get to close
+		checkPPoints ();
 
-		yVal = (camDistance) * Mathf.Sin (cameraAngle);
+		if (!lerping) {
+			transform.forward = closestPoint.GetComponent<PPointScript> ().direction;
 
-		xzVal = (camDistance) * Mathf.Cos (cameraAngle);
+			transform.position = median + -(closestPoint.GetComponent<PPointScript> ().direction * closestPoint.GetComponent<PPointScript> ().distance);
+		}
+	}
 
-		Vector3 projectedCam = median + (cross*-xzVal);
+	void checkPPoints(){
+		GameObject[] points = GameObject.FindGameObjectsWithTag ("PPoint");
 
-		Debug.DrawLine(median, projectedCam, Color.blue);
-		Debug.DrawLine(projectedCam, projectedCam+new Vector3(0,yVal,0), Color.yellow);
-
-		transform.position = new Vector3 (projectedCam.x, median.y + yVal, projectedCam.z);
-
-		transform.forward = Vector3.Normalize ((median - transform.position));
-
-		//transform.position = new Vector3(
-		//	median + (cross* -distance/4)
-		transform.position += transform.up * 2f;
+		float minLength = float.MaxValue;
+		GameObject tempClosestPoint = null;
+		foreach(GameObject point in points){
+			Debug.DrawLine(point.transform.position, median, Color.blue);
+			if((median-point.transform.position).magnitude<minLength){
+				minLength=(median-point.transform.position).magnitude;
+				tempClosestPoint= point;
+			}
+		}
+		if (closestPoint != tempClosestPoint&& !lerping) {
+			if(closestPoint!=null){
+				lerping = true;
+				timerStart = Time.time;
+			}
+			prevClosestPoint = closestPoint;
+			closestPoint = tempClosestPoint;
+		}
 	}
 }
