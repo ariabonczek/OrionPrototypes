53b946a97ee6d8873a44a0b4e5a196c5da4563d0
unknown <sbh9278@IGMResearch-06.main.ad.rit.edu>
2015-11-24 00:00:46 -0500

    Added a new prototype, added Emma to both current prototypes.

---
+++ BridgePrototype/Assets/scripts/Camera Scripts/PPointScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PPointScript : MonoBehaviour {
+	// The Vector 3 representing the direction of the camera when in this point's zone.
+	public Vector3 direction;
+	// The float representing the magnitude of the camera's "arm"
+	public float distance;
+
+	// Use this for initialization
+	void Start () {
+		direction.Normalize ();
+	}
+}
---
+++ BridgePrototype/Assets/scripts/Environment Scripts/RespawnHandle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnHandle : MonoBehaviour {
+
+	public GameObject player1Respawn;
+	public GameObject player2Respawn;
+
+	private bool play1, play2;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.name == "Player 1")
+		{
+			play1 = true;
+		}
+		else if (col.gameObject.name == "Player 2")
+		{
+			play2 = true;
+		}
+
+		if (play1 && play2)
+		{
+			GameObject.Find("Player 1").GetComponent<Player1Script>().RespawnPosition = player1Respawn.transform.position;
+			GameObject.Find("Player 2").GetComponent<Player1Script>().RespawnPosition = player2Respawn.transform.position;
+			Destroy(transform.gameObject);
+		}
+	}
+}
